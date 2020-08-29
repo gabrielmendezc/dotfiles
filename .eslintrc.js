ffb556bca358d24d948c15cb2b058cccd5a1f8f4
Gabriel Mendez <yo@gabrielmendezc.com>
2020-08-29 21:45:39 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'kentcdodds/jest'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+}
